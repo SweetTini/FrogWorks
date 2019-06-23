d7351e1fe11a3bd9fe823aa37eb925eb1966ebe3
bjammart <bjammart@yahoo.com>
2019-06-23 16:20:35 -0400

    Add project files

---
+++ FrogWorks/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XnaGame = Microsoft.Xna.Framework.Game;
+
+namespace FrogWorks
+{
+    public class Game : XnaGame
+    {
+
+    }
+}
