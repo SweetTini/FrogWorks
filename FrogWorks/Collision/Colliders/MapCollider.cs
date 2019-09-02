b585a7e35268ca65198ecc144ba86c23346e0b46
mbenjamin <mbenjamin@instechnologies.net>
2019-09-02 14:52:06 -0400

    Fix namespace for map collider

--- FrogWorks/Collision/Colliders/MapCollider.cs
+++ FrogWorks/Collision/Colliders/MapCollider.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 
-namespace FrogWorks.Collision.Colliders
+namespace FrogWorks
 {
     public abstract class MapCollider<T> : Collider
     {
