59759192145d75791cab4200d91ad7f9da971f9f
mbenjamin <mbenjamin@instechnologies.net>
2019-09-13 23:35:32 -0400

    Cleanup moveable entity logic for readability

--- FrogWorks.Demo/Entities/MoveableEntity.cs
+++ FrogWorks.Demo/Entities/MoveableEntity.cs
@@ -25,8 +25,6 @@ namespace FrogWorks.Demo.Entities
 
         public bool IsOnRightWall { get; private set; }
 
-        public bool IsOnWall => IsOnLeftWall || IsOnRightWall;
-
         public bool IsOnCeiling { get; private set; }
 
         public bool IsOnGround { get; private set; }
@@ -51,66 +49,60 @@ namespace FrogWorks.Demo.Entities
 
         private void UpdatePhysics()
         {
+            MoveAndResolveCollisionHorizontally();
+            MoveAndResolveCollisionVertically();
+            DetectEdges();
+        }
+
+        private void MoveAndResolveCollisionHorizontally()
+        {
             X += XVelocity;
-            CheckHorizontalCollision();
+
+            if (!IsCollidable || XVelocity == 0f) return;
+
+            var direction = XVelocity.Sign();
+            var offset = direction > 0f ? Right - 1 : Left;
+
+            if (World.IsSolid(offset, Top, 1, Height))
+            {
+                var gridOffset = direction > 0f ? (World.CellWidth - Width).Mod(World.CellWidth) : 0f;
+                var gridPosition = direction > 0f ? (X / World.CellWidth).Floor() : (X / World.CellWidth).Ceiling();
+                X = World.Absolute.X + gridPosition * World.CellWidth + gridOffset;
+                XVelocity = 0f;
+            }
+        }
+
+        private void MoveAndResolveCollisionVertically()
+        {
             Y += YVelocity;
-            CheckVerticalCollision();
+
+            if (!IsCollidable || YVelocity == 0f) return;
+
+            var direction = YVelocity.Sign();
+            var offset = direction > 0f ? Bottom - 1 : Top;
+            var isAboveJumpThru = false;
+
+            if (direction > 0f)
+                isAboveJumpThru = World.IsAboveJumpThru(Left, offset, Width, 1, XVelocity, YVelocity);
+
+            if (World.IsSolid(Left, offset, Width, 1) || isAboveJumpThru)
+            {
+                var gridOffset = direction > 0f ? (World.CellHeight - Height).Mod(World.CellHeight) : 0f;
+                var gridPosition = direction > 0f ? (Y / World.CellHeight).Floor() : (Y / World.CellHeight).Ceiling();
+                Y = World.Absolute.Y + gridPosition * World.CellHeight + gridOffset;
+                YVelocity = 0f;
+            }
         }
 
-        private void CheckHorizontalCollision()
+        private void DetectEdges()
         {
             if (!IsCollidable) return;
 
-            if (XVelocity > 0)
-            {
-                if (World.IsSolid(Right - 1, Top, 1, Height))
-                {
-                    var offset = (World.CellWidth - Width).Mod(World.CellWidth);
-                    X = World.Absolute.X + (X / World.CellWidth).Floor() * World.CellWidth + offset;
-                    XVelocity = 0f;
-                }
-            }
-            else if (XVelocity < 0)
-            {
-                if (World.IsSolid(Left, Top, 1, Height))
-                {
-                    X = World.Absolute.X + (X / World.CellWidth).Ceiling() * World.CellWidth;
-                    XVelocity = 0f;
-                }
-            }
-
-            IsOnRightWall = World.IsSolid(Right, Top, 1, Height);
             IsOnLeftWall = World.IsSolid(Left - 1, Top, 1, Height);
-        }
-
-        private void CheckVerticalCollision()
-        {
-            if (!IsCollidable) return;
-
-            if (YVelocity > 0)
-            {
-                var isOnPlaform = World.IsSolid(Left, Bottom - 1, Width, 1) 
-                    || World.IsAboveJumpThru(Left, Bottom - 1, Width, 1, XVelocity, YVelocity);
-
-                if (isOnPlaform)
-                {
-                    var offset = (World.CellHeight - Height).Mod(World.CellHeight);
-                    Y = World.Absolute.Y + (Y / World.CellHeight).Floor() * World.CellHeight + offset;
-                    YVelocity = 0f;
-                }
-            }
-            else if (YVelocity < 0)
-            {
-                if (World.IsSolid(Left, Top, Width, 1))
-                {
-                    Y = World.Absolute.Y + (Y / World.CellHeight).Ceiling() * World.CellHeight;
-                    YVelocity = 0f;
-                }
-            }
-
-            IsOnGround = World.IsSolid(Left, Bottom, Width, 1) 
-                || (World.IsJumpThru(Left, Bottom, Width, 1) && YVelocity >= 0f);
+            IsOnRightWall = World.IsSolid(Right, Top, 1, Height);
             IsOnCeiling = World.IsSolid(Left, Top - 1, Width, 1);
+            IsOnGround = World.IsSolid(Left, Bottom, Width, 1) || (YVelocity >= 0f 
+                && World.IsJumpThru(Left, Bottom, Width, 1));
         }
 
         public override string ToString()
