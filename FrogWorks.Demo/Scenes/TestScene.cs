46b2852c375978cdea2351c02a43ab675d2ea8e4
mbenjamin <mbenjamin@instechnologies.net>
2019-10-28 00:23:00 -0400

    Modify demo to test new layers.

--- FrogWorks.Demo/Scenes/TestScene.cs
+++ FrogWorks.Demo/Scenes/TestScene.cs
@@ -15,15 +15,22 @@ namespace FrogWorks.Demo.Scenes
             //var mapTest = Tiled.Load("Maps\\TestArea1.tmx");
             BackgroundColor = Color.CornflowerBlue;
 
+            var testEffect = ShaderEffect.Load("Effects/SampleEffect.mgfxo");
+
             var backgroundLayer = new BasicLayer() { ScrollRate = Vector2.One * .4f };
+            var shaderLayer = new ShaderMaskLayer(testEffect, true) { ScrollRate = Vector2.Zero };
             var mainLayer = new BasicLayer();
             var hudLayer = new BasicLayer() { ScrollRate = Vector2.Zero };
-            Layers.Add(backgroundLayer, mainLayer, hudLayer);
+
+            Layers.Add(backgroundLayer, shaderLayer, mainLayer, hudLayer);
 
             var checker = new Checker();
             var world = new World(30, 10, 32, 32);
             var player = new Player(world) { X = 64f, Y = 64f };
+            var transition = new Transition() { X = 160f, Y = 120f };
+
             backgroundLayer.Entities.Add(checker);
+            shaderLayer.Entities.Add(transition);
             mainLayer.Entities.Add(world, player);
             Camera.SetZone(world.Size.ToPoint());
 
