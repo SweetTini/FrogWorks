99e280da7f15ddbde220fc1e34f930670bf159dc
mbenjamin <mbenjamin@instechnologies.net>
2019-11-24 22:40:38 -0500

    Update Map class to include point options.

--- FrogWorks/Utils/Map.cs
+++ FrogWorks/Utils/Map.cs
@@ -1,41 +1,50 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 
 namespace FrogWorks
 {
     public class Map<T>
     {
-        private T[,] _area;
+        private T[,] _region;
 
         public T this[int x, int y]
         {
-            get { return _area.WithinRange(x, y) ? _area[x, y] : Empty; }
+            get { return _region.WithinRange(x, y) ? _region[x, y] : Empty; }
             set
             {
-                if (!_area.WithinRange(x, y)) return;
+                if (!_region.WithinRange(x, y)) return;
                 if (value == null && !value.Equals(Empty)) value = Empty;
-                _area[x, y] = value;
+                _region[x, y] = value;
             }
         }
 
-        public int Columns { get; private set; }
+        public Point Size { get; private set; }
 
-        public int Rows { get; private set; }
+        public int Columns => Size.X;
+
+        public int Rows => Size.Y;
+
+        public int Area => Size.X * Size.Y;
 
         public T Empty { get; private set; }
 
-        public Map(int columns, int rows, T empty = default(T))
+        public Map(Point size, T empty = default(T))
         {
-            Columns = columns;
-            Rows = rows;
+            Size = size.Abs();
             Empty = empty;
-            _area = new T[Columns, Rows];
+            _region = new T[Columns, Rows];
 
-            for (int i = 0; i < Columns * Rows; i++)
+            for (int i = 0; i < Area; i++)
             {
                 var x = i % Columns;
                 var y = i / Columns;
-                _area[x, y] = Empty;
+                _region[x, y] = Empty;
             }
+        }
+
+        public Map(int columns, int rows, T empty = default(T))
+            : this(new Point(columns, rows), empty)
+        {
         }
 
         public Map(T[,] map, T empty = default(T))
@@ -48,7 +57,7 @@ namespace FrogWorks
             {
                 var x = i % columns;
                 var y = i / columns;
-                _area[x, y] = map[x, y];
+                _region[x, y] = map[x, y];
             }
         }
 
@@ -58,52 +67,64 @@ namespace FrogWorks
             {
                 var x = i % Columns;
                 var y = i / Columns;
-                _area[x, y] = Empty;
+                _region[x, y] = Empty;
             }
+        }
+
+        public void Resize(Point size)
+        {
+            Resize(Point.Zero, size.Abs());
         }
 
         public void Resize(int columns, int rows)
         {
-            Resize(0, 0, columns, rows);
+            Resize(Point.Zero, new Point(columns, rows).Abs());
+        }
+
+        public void Resize(Point from, Point to)
+        {
+            var size = (to - from).Abs();
+            var region = new T[size.X, size.Y];
+
+            for (int i = 0; i < size.X * size.Y; i++)
+            {
+                var x = i % size.X;
+                var y = i / size.X;
+
+                region[x, y] = _region.WithinRange(from.X + x, from.Y + y)
+                             ? _region[from.X + x, from.Y + y] : Empty;
+            }
+
+            _region = region;
+            Size = size;
         }
 
         public void Resize(int x1, int y1, int x2, int y2)
         {
-            var columns = Math.Abs(x2 - x1);
-            var rows = Math.Abs(y2 - y1);
-            var area = new T[columns, rows];
-
-            for (int i = 0; i < columns * rows; i++)
-            {
-                var x = i % columns;
-                var y = i / columns;
-                area[x, y] = _area.WithinRange(x + x1, y + y1) ? _area[x + x1, y + y1] : Empty;
-            }
-
-            _area = area;
-            Columns = columns;
-            Rows = rows;
+            Resize(new Point(x1, y1), new Point(x2, y2));
         }
 
-        public bool IsEmpty(int x, int y) => _area[x, y].Equals(Empty);
+        public bool IsEmpty(Point cell) => _region[cell.X, cell.Y].Equals(Empty);
+
+        public bool IsEmpty(int x, int y) => IsEmpty(new Point(x, y));
 
         public Map<T> Clone()
         {
-            return new Map<T>(_area, Empty);
+            return new Map<T>(_region, Empty);
         }
 
         public T[,] ToArray()
         {
-            var area = new T[Columns, Rows];
+            var region = new T[Columns, Rows];
 
-            for (int i = 0; i < Columns * Rows; i++)
+            for (int i = 0; i < Area; i++)
             {
                 var x = i % Columns;
                 var y = i / Columns;
-                area[x, y] = _area[x, y];
+                region[x, y] = _region[x, y];
             }
 
-            return area;
+            return region;
         }
     }
 }
