9f3d5db77816e6da7744ae69ed1f0fa2a0c43bdf
Sweet Tini <mbenjamin@instechnologies.net>
2020-02-09 22:28:17 -0500

    Condense TexturePacker's Read methods into a single method.

--- FrogWorks/Assets/Loaders/TexturePacker.cs
+++ FrogWorks/Assets/Loaders/TexturePacker.cs
@@ -21,34 +21,31 @@ namespace FrogWorks
                 using (var stream = File.OpenRead(absolutePath))
                 {
                     var atlas = new TextureAtlas();
-                    Read(stream, atlas, Path.GetDirectoryName(filePath));
+                    var document = new XmlDocument();
+                    document.Load(stream);
+
+                    var root = document["TextureAtlas"];
+                    var directory = Path.GetDirectoryName(filePath);
+                    var imagePath = Path.Combine(directory, root.AttrToString("imagePath"));
+                    var rootTexture = Texture.Load(imagePath);
+
+                    foreach (XmlElement spriteNode in root.ChildNodes)
+                    {
+                        var key = Path.GetFileNameWithoutExtension(spriteNode.AttrToString("n"));
+                        var texture = rootTexture.ClipRegion(spriteNode.AttrToRectangle("x", "y", "w", "h"));
+                        var size = spriteNode.AttrToVector2("oW", "oH");
+                        var origin = spriteNode.AttrToVector2("oX", "oY");
+                        var isRotated = spriteNode.HasAttribute("r");
+
+                        atlas.Add(key, new TextureAltasTexture(texture, size, origin, isRotated));
+                    }
+
                     return atlas;
                 }
             }
             catch
             {
                 return null;
-            }
-        }
-
-        private static void Read(FileStream stream, TextureAtlas atlas, string directory)
-        {
-            var document = new XmlDocument();
-            document.Load(stream);
-
-            var root = document["TextureAtlas"];
-            var imagePath = Path.Combine(directory, root.AttrToString("imagePath"));
-            var rootTexture = Texture.Load(imagePath);
-
-            foreach (XmlElement spriteNode in root.ChildNodes)
-            {
-                var key = Path.GetFileNameWithoutExtension(spriteNode.AttrToString("n"));
-                var texture = rootTexture.ClipRegion(spriteNode.AttrToRectangle("x", "y", "w", "h"));
-                var size = spriteNode.AttrToVector2("oW", "oH");
-                var origin = spriteNode.AttrToVector2("oX", "oY");
-                var isRotated = spriteNode.HasAttribute("r");
-
-                atlas.Add(key, new TextureAltasTexture(texture, size, origin, isRotated));
             }
         }
     }
